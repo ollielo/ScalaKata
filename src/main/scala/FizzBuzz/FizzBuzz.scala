54def204f218052fd56e9406aa98e70e158c4dd8
Li-Ta Lo <lita.ollie@gmail.com>
2014-02-22 08:26:37 -0700

    add first failing test for FizzBuzz

---
+++ src/main/scala/FizzBuzz/FizzBuzz.scala
@@ -0,0 +1,7 @@
+package main.scala.FizzBuzz
+
+object FizzBuzz {
+  def toString(numbers: List[Int]): List[String] = {
+    List()
+  }
+}
---
+++ src/test/scala/FizzBuss/FizzBuzzSuite.scala
@@ -0,0 +1,11 @@
+package test.scala.FizzBuss
+
+import test.scala.UnitSuite
+import main.scala.FizzBuzz.FizzBuzz
+
+class FizzBuzzSuite extends UnitSuite {
+  test("We know how to convert a List of Integers into a String") {
+    var numbers = (1 to 10).toList
+    assertResult(List(1, 2, 3, 4, 5))(FizzBuzz.toString(numbers))
+  }
+}
