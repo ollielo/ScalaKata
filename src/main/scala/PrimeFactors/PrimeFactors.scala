e5225cd095969184900f3994c7ab2fb4743fd64c
Li-Ta Lo <lita.ollie@gmail.com>
2014-02-23 14:29:05 -0700

    extract the general case into a local recursive function as "loop".

--- src/main/scala/PrimeFactors/PrimeFactors.scala
+++ src/main/scala/PrimeFactors/PrimeFactors.scala
@@ -4,13 +4,16 @@ object PrimeFactors {
   def apply(number: Int): List[Int] = {
     require(number > 0)
 
-    number match {
-      case 1 => List()
-      case _ => if (number % 2 == 0) {
+    def loop: List[Int] = {
+      if (number % 2 == 0) {
         2 :: PrimeFactors(number / 2)
       } else {
         3 :: PrimeFactors(number / 3)
       }
+    }
+    number match {
+      case 1 => List()
+      case _ => loop
     }
   }
 }
