edd078b2c75dd91ad339ed12dcf6349a254b27ee
Li-Ta Lo <lita.ollie@gmail.com>
2014-02-23 09:35:44 -0700

    add the requirement that the argument to PrimeFactor needs to be larger than 0.

--- src/main/scala/PrimeFactors/PrimeFactors.scala
+++ src/main/scala/PrimeFactors/PrimeFactors.scala
@@ -2,6 +2,8 @@ package main.scala.PrimeFactors
 
 object PrimeFactors {
   def apply(number: Int): List[Int] = {
+    require(number > 0)
+
     List()
   }
 }
--- src/test/scala/PrimeFactors/PrimeFactorsSuite.scala
+++ src/test/scala/PrimeFactors/PrimeFactorsSuite.scala
@@ -4,6 +4,11 @@ import test.scala.UnitSuite
 import main.scala.PrimeFactors.PrimeFactors
 
 class PrimeFactorsSuite extends UnitSuite {
+  test("0 or negative numbers are not valid input") {
+    intercept[IllegalArgumentException] {
+      PrimeFactors(0)
+    }
+  }
   test("1 does not have any prime factors") {
     assertResult(Nil)(PrimeFactors(1))
   }
