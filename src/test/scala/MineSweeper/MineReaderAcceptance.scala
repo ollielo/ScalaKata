b8d8787decf935854fe7e1b3b264520c61c0d868
Li-Ta Lo <lita.ollie@gmail.com>
2014-07-13 14:54:46 -0600

    add test on the size of the second field in the acceptance input

--- src/test/scala/MineSweeper/MineReaderAcceptance.scala
+++ src/test/scala/MineSweeper/MineReaderAcceptance.scala
@@ -7,22 +7,23 @@ import main.scala.MineSweeper.MineReader
 class MineReaderAcceptance extends UnitSuite with BeforeAndAfterEach {
   val dir = "src/test/resources/"
   val maybeFields = MineReader(dir + "minesweeper_acceptance.txt")
-
-  test("Acceptance input has two MineFields") {
-    maybeFields match {
-      case Some(fields) =>
-        assertResult(2)(fields.length)
-      case None => fail("Expected Some(List[Field]), got None")
-    }
+  val fields = maybeFields match {
+    case Some(fields) => fields
+    case None         => fail("Expected Some(List[Field]), got None")
   }
 
-  test("The first filed in the accpetance input is 4 by 4") {
-    maybeFields match {
-      case Some(fields) =>
-        val field = fields.head
-        assertResult((4, 4))(field.dims)
-      case None => fail("Expected Some(List[Field]), got None")
-    }
+  test("Acceptance input has two MineFields") {
+    assertResult(2)(fields.length)
+  }
+
+  test("The first filed is 4 by 4") {
+    val field = fields.head
+    assertResult((4, 4))(field.dims)
+  }
+
+  test("The second field is 3 by 5") {
+    val field = fields(1)
+    assertResult((3, 5))(field.dims)
   }
 
 }
