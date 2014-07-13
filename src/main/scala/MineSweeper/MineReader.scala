4c12356bec0f09fe11d06d8558c684c824d6dbfb
Li-Ta Lo <lita.ollie@gmail.com>
2014-07-13 10:42:37 -0600

    pass 0x0 field, added test on 1by1, refactore the directory name for input files.

--- src/main/scala/MineSweeper/MineReader.scala
+++ src/main/scala/MineSweeper/MineReader.scala
@@ -1,9 +1,18 @@
 package main.scala.MineSweeper
 
-class MineField
+import scala.io.Source
+
+class MineField {
+  val dims = (0, 0)
+}
 
 object MineReader {
-  def apply(filename: String): Option[MineField] = {
-    None
+  def apply(filename: String): Option[List[MineField]] = {
+    val lines = Source.fromFile(filename).getLines.toList
+    if (lines == Nil) {
+      None
+    } else {
+      Some(Nil)
+    }
   }
 }
--- src/test/scala/MineSweeper/MineReaderSuite.scala
+++ src/test/scala/MineSweeper/MineReaderSuite.scala
@@ -2,13 +2,21 @@ package test.scala.MineSweeper
 
 import test.scala.UnitSuite
 import main.scala.MineSweeper.MineReader
+import java.io.File
+import org.scalatest.Ignore
 
 class MineReaderSuite extends UnitSuite {
-  test("Reading an empty file returns Nothing") {
-    assertResult(None)(MineReader("minesweeper_empty.txt"))
+  val dir = "src/test/resources/"
+
+  test("Reading an empty file returns None") {
+    assertResult(None) {
+      MineReader(dir + "minesweeper_empty.txt")
+    }
   }
 
-  test("Reading a input file with only 0 0 return an empty List") {
-    assertResult(Some(Nil))(MineReader("minesweeper_0by0.txt"))
+  test("Reading an input file with only 0 0 return an empty List") {
+    assertResult(Some(Nil)) {
+      MineReader(dir + "minesweeper_0by0_field.txt")
+    }
   }
 }
