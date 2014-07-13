1464cb5bbbc60b56d7ddb6268d698934bf89897c
Li-Ta Lo <lita.ollie@gmail.com>
2014-07-13 15:20:59 -0600

    refactor MineReader to ues a private factory method for MineField

--- src/test/scala/MineSweeper/MineReaderUnit.scala
+++ src/test/scala/MineSweeper/MineReaderUnit.scala
@@ -4,9 +4,18 @@ import test.scala.UnitSuite
 import main.scala.MineSweeper.MineReader
 import java.io.File
 import org.scalatest.Ignore
+import main.scala.MineSweeper.MineField
 
 class MineReaderUnit extends UnitSuite {
   val dir = "src/test/resources/"
+
+  private def getMineField(filename: String): MineField = {
+    val maybeFields = MineReader(dir + "minesweeper_1by1_mine.txt")
+    maybeFields match {
+      case Some(fields) => fields.head
+      case None         => fail("Expected Some(List[Field]), got None")
+    }
+  }
 
   test("Reading an empty file returns None") {
     assertResult(None) {
@@ -21,23 +30,13 @@ class MineReaderUnit extends UnitSuite {
   }
 
   test("Reading an input file with only one mine returns a List with a 1 by 1 field") {
-    val maybeFields = MineReader(dir + "minesweeper_1by1_mine.txt")
-    maybeFields match {
-      case Some(fields) =>
-        val field = fields.head
-        assertResult((1, 1))(field.dims)
-      case None => fail("Expected Some(List[Field]), got None")
-    }
+    val field = getMineField("minesweeper_1by1_mine.txt")
+    assertResult((1, 1))(field.dims)
   }
 
   test("Reading an input file with only one mine returns a field with a mine at (0, 0)") {
-    val maybeFields = MineReader(dir + "minesweeper_1by1_mine.txt")
-    maybeFields match {
-      case Some(fields) =>
-        val field = fields.head
-        assertResult(Some('*'))(field(0, 0))
-      case None => fail("Expected Some(List[Field]), got None")
-    }
+    val field = getMineField("minesweeper_1by1_mine.txt")
+    assertResult(Some('*'))(field(0, 0))
   }
 }
 
