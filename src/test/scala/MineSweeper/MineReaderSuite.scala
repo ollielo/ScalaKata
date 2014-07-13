5f7efd6283598eaede686c958201b50a98d57c80
Li-Ta Lo <lita.ollie@gmail.com>
2014-07-13 09:47:24 -0600

    Add test on 0x0 input file. Failed to return Some(Nil).

--- src/test/scala/MineSweeper/MineReaderSuite.scala
+++ src/test/scala/MineSweeper/MineReaderSuite.scala
@@ -7,4 +7,8 @@ class MineReaderSuite extends UnitSuite {
   test("Reading an empty file returns Nothing") {
     assertResult(None)(MineReader("minesweeper_empty.txt"))
   }
+
+  test("Reading a input file with only 0 0 return an empty List") {
+    assertResult(Some(Nil))(MineReader("minesweeper_0by0.txt"))
+  }
 }
