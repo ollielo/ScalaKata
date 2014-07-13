1037959932aa075e49b6071e991ac049bc7c4f11
Li-Ta Lo <lita.ollie@gmail.com>
2014-07-13 12:06:29 -0600

    fixed the ill formed 1 by 1 input file
    addd and refactored MineField factory method
    
    and MineField companion object

--- src/main/scala/MineSweeper/MineReader.scala
+++ src/main/scala/MineSweeper/MineReader.scala
@@ -3,21 +3,36 @@ package main.scala.MineSweeper
 import scala.io.Source
 
 class MineField(val dims: Tuple2[Int, Int] = (0, 0)) {
+  def this(lines: Array[String]) = {
+    this((lines.length, lines(0).length))
+  }
+}
 
+object MineField {
+  def apply(lines: List[String]): MineField = {
+    new MineField(lines.toArray)
+  }
 }
 
 object MineReader {
+  def mineFieldFactor(lines: List[String]): List[MineField] = {
+    val dims = lines.head.split(" ").map(x => x.toInt)
+    dims match {
+      case Array(0, 0) => Nil
+      case Array(nrows, ncols) =>
+        val first = lines.tail.take(nrows)
+        val rest = lines.tail.drop(nrows)
+        MineField(first) :: mineFieldFactor(lines.tail.drop(nrows))
+      case _ => Nil
+    }
+  }
+
   def apply(filename: String): Option[List[MineField]] = {
     val lines = Source.fromFile(filename).getLines.toList
     if (lines == Nil) {
       None
     } else {
-      val dims = lines.head.split(" ").map(x => x.toInt)
-      dims match {
-        case Array(0, 0)         => Some(Nil)
-        case Array(nrows, ncols) => Some(List(new MineField((nrows, ncols))))
-        case _                   => None
-      }
+      Some(mineFieldFactor(lines))
     }
   }
 }
