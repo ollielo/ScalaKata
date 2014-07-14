8285309d9cdc68c01a7b96ab9339ed1af6395020
Li-Ta Lo <lita.ollie@gmail.com>
2014-07-13 18:40:17 -0600

    add the all suite suites for MineSweeper

---
+++ src/test/scala/MineSweeper/MineSweeperSuites.scala
@@ -0,0 +1,9 @@
+package test.scala.MineSweeper
+
+import org.scalatest.Suites
+import org.scalatest.ParallelTestExecution
+
+class MineSweeperSuites extends Suites(
+  new MineFieldUnit,
+  new MineReaderUnit,
+  new MineReaderAcceptance) with ParallelTestExecution
