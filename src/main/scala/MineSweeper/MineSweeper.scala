fe4c1eb9e04bd41e3e7b2b6beb323d7cd1788ee8
Li-Ta Lo <lita.ollie@gmail.com>
2014-07-15 20:55:57 -0600

    add the acceptance test application

--- src/main/scala/MineSweeper/MineSweeper.scala
+++ src/main/scala/MineSweeper/MineSweeper.scala
@@ -1,7 +1,23 @@
 package main.scala.MineSweeper
 
 object MineSweeper {
-  def apply(field: String): String = {
-    new String()
+  def apply(filename: String): String = {
+    val maybeFields = MineReader(filename)
+
+    maybeFields match {
+      case Some(fields) => (fields zip (1 to fields.length)).map {
+        case (field, i) => {
+          "Field #" + i + ":\n" +
+            MineSolver(field).toString() +
+            "\n\n"
+        }
+      }.reduce(_ + _)
+      case _ => ""
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val sol = MineSweeper("src/test/resources/minesweeper_acceptance.txt")
+    println(sol)
   }
 }
