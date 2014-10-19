b147f1f008e15b82b856a64be00935916cfecd21
Li-Ta Lo <lita.ollie@gmail.com>
2014-10-19 07:49:26 -0600

    add UnitSpec as base class for unit tests with FunSpec an Matchers, Add failing test for StringCalculator

---
+++ src/test/scala/StringCalculator/StringCalculatorSpec.scala
@@ -0,0 +1,11 @@
+package test.scla.StringCalculator
+
+import test.scala.UnitSpec
+
+class StringCalculatorSpec extends UnitSpec {
+  describe("StringCalculator") {
+    it("is a failure") {
+      fail("yes it is")
+    }
+  }
+}
---
+++ src/test/scala/UnitSpec.scala
@@ -0,0 +1,5 @@
+package test.scala
+
+import org.scalatest.{FunSpec, Matchers}
+
+class UnitSpec extends FunSpec with Matchers
