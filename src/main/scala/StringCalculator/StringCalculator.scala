72bca41c22ce4933a9eda9dc92cdc2bafe09ccd6
Li-Ta Lo <lita.ollie@gmail.com>
2014-10-19 07:57:13 -0600

    add test case and implementation on empty string.

---
+++ src/main/scala/StringCalculator/StringCalculator.scala
@@ -0,0 +1,5 @@
+package main.scala.StringCalculator
+
+object StringCalculator {
+  def apply(input: String): Int = 0
+}
--- src/test/scala/StringCalculator/StringCalculatorSpec.scala
+++ src/test/scala/StringCalculator/StringCalculatorSpec.scala
@@ -1,11 +1,12 @@
 package test.scla.StringCalculator
 
+import main.scala.StringCalculator.StringCalculator
 import test.scala.UnitSpec
 
 class StringCalculatorSpec extends UnitSpec {
   describe("StringCalculator") {
-    it("is a failure") {
-      fail("yes it is")
+    it("Should return 0 for an empty String") {
+      StringCalculator("") should equal(0)
     }
   }
 }
