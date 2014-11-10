a0b3f62e77b634599cc66164716dcc35422eac2b
Li-Ta Lo <lita.ollie@gmail.com>
2014-11-10 16:14:40 -0700

    initial import of GildedRose Kata
     Import Scala version of GildedRose Kata from Emily Bache's github repo

---
+++ src/main/scala/GildedRose/Item.scala
@@ -0,0 +1,5 @@
+package main.scala.GildedRose
+
+class Item(val name: String, var sellIn: Int, var quality: Int) {
+
+}
---
+++ src/test/scala/GildedRose/GildedRoseTest.scala
@@ -0,0 +1,13 @@
+package test.scala.GildedRose
+
+import main.scala.GildedRose.{GildedRose, Item}
+import org.scalatest._
+
+class GildedRoseTest  extends FlatSpec with Matchers {
+      it should "foo" in {
+        var items = Array[Item](new Item("foo", 0, 0))
+        val app = new GildedRose(items)
+        app.updateQuality()
+        (app.items(0).name) should equal ("fixme")
+      }
+}
